c14aeaf702799f199dcf5b70a6187f046a8d2df9
NanaLuv <lovenana961@gmail.com>
2026-04-16 18:24:53 +0000

    modified .env to add deployment env variables

--- server/db.js
+++ server/db.js
@@ -2,15 +2,17 @@ const mysql = require("mysql2/promise");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const urlDB = process.env.MYSQL_URL || process.env.MYSQL_PUBLIC_URL
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || "sch_mgt",
-  port: process.env.DB_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  uri: urlDB,
+  // host: process.env.DB_HOST || "localhost",
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME || "sch_mgt",
+  // port: process.env.DB_PORT || 3306,
+  // waitForConnections: true,
+  // connectionLimit: 10,
+  // queueLimit: 0,
 });
 
 module.exports = pool
