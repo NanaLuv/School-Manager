199caf7070dd64b0a91ebd4051ff1a2d14f0caa1
NanaLuv <lovenana961@gmail.com>
2026-04-17 04:25:18 +0000

    insert mysql url for deployment

--- server/db.js
+++ server/db.js
@@ -2,7 +2,6 @@ const mysql = require("mysql2/promise");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const connectionUrl = process.env.MYSQL_PUBLIC_URL;
-const pool = mysql.createPool(connectionUrl);
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = pool;
