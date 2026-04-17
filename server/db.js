20dc35ab7be028665b7e04cbf72b488c6918c259
NanaLuv <lovenana961@gmail.com>
2026-04-17 03:41:41 +0000

    Use MYSQL_PUBLIC_URL for Railway connection

--- server/db.js
+++ server/db.js
@@ -2,9 +2,7 @@ const mysql = require("mysql2/promise");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const MYSQL_PUBLIC_URL =
-  "mysql://${process.env.MYSQLUSER}:${process.env.MYSQL_ROOT_PASSWORD}@${process.env.RAILWAY_TCP_PROXY_DOMAIN}}:${process.env.RAILWAY_TCP_PROXY_PORT}/${process.env.MYSQL_DATABASE}";
-
-const pool = mysql.createPool(MYSQL_PUBLIC_URL);
+const connectionUrl = process.env.MYSQL_PUBLIC_URL;
+const pool = mysql.createPool(connectionUrl);
 
 module.exports = pool;
